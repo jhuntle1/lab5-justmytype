ebe964f0e051668d8a17e4286c95fffe7099a47a
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-28 09:35:59 -0500

    sentence at top, keypad working

--- justmytype.js
+++ justmytype.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {
+var sentences = [
+    ' ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'
+];
+sentenceCounter = 0;
+// var moveBy = "2px";
+// var sentenceDiv = document.createElement('div');
+//     sentenceDiv.className = 'divvy';
+//     sentenceDiv.innerText = 'Click Me';
+//     sentenceDiv.addEventListener('keydown', moveBy);
+
+
+// var textCorrect = 'keydown'
+// if (textCorrect==true){ 
+//     function next(){ 
+//         $('letter').removeClass('yellow'); 
+//         $('nextLetter').addClass('yellow'); 
+//     } 
+// } 
+
+$(document).ready(function () {
     var upper = $('#keyboard-upper-container'),
         lower = $('#keyboard-lower-container');
 
     upper.hide();
+    $('#sentence').append(sentences[sentenceCounter]);
 
-    $(document).on('keyup keydown', function(e) {
+    $(document).on('keyup keydown', function (e) {
         if (e.shiftKey) {
             lower.hide();
             upper.show();
@@ -18,9 +42,14 @@ $(document).ready(function() {
         } else {
             handleHighlight(e, '#f5f5f5');
         }
+    })
+})
 
-    });
-});
+function handleSentences() {
+    $('#sentence').empty();
+    $('#sentence').append(sentences[sentenceCounter]);
+    sentenceCounter++
+}
 
 function handleHighlight(e, color) {
     var code = e.keyCode;
@@ -34,24 +63,25 @@ function handleHighlight(e, color) {
     } else {
         $('span:contains(' + e.key + ')').css('background-color', color);
     }
+    
 }
 
 var utils = {
-    isAlphanumeric: function(code) {
-        if ((code >= 65 && code <= 90) || (code >= 48 && code <= 57) || (code=32)) {
+    isAlphanumeric: function (code) {
+        if ((code >= 65 && code <= 90) || (code >= 48 && code <= 57) || (code==32)) {
             return true;
         } else {
             return false;
         }
     },
-    isNumber: function(code) {
+    isNumber: function (code) {
         if (code >= 48 && code <= 57) {
             return true;
         } else {
             return false;
         }
     },
-    isAlpha: function(code) {
+    isAlpha: function (code) {
         if (code >= 65 && code <= 90) {
             return true;
         } else {
